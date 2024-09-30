0ff2018b4c653ba91cbbb7322ece5be448032335
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2024-09-30 17:51:50 +0530

    Add public url

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-// async function getNames(): Promise<string[]> {
-//   const names = await axios.get(`${process.env.DOMAIN}/api/names`).then((res) => res.data);
-//   return names;
-// }
+async function getNames(): Promise<string[]> {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+  const names = await fetch(baseUrl + '/api/names').then((res) => res.json());
+
+  return names;
+}
 
 export default async function Home() {
-  const names = ['Hello', 'World'];
+  const names = await getNames();
 
   return (
     <div className="h-screen flex items-center justify-center">
